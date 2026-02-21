987b7a36f75565268355d176fe7494f2ddd0eebf
nominenkh <nominenkhmunkh@gmail.com>
2026-02-21 18:08:09 +0100

    Create 00_setup.R

---
+++ code/00_setup.R
@@ -0,0 +1,11 @@
+
+# Master environment setup for thesis replication
+
+
+library(readxl)   
+library(dplyr)     
+library(tidyr)     
+library(plm)      
+library(lmtest)    
+library(sandwich)  
+library(stargazer) 
